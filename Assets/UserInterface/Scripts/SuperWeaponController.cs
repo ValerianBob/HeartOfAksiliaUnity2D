0096d50d0960719e580107025205de07498f01b1
Valerian <walerian813@gmail.com>
2024-11-08 21:02:22 +0200

    Add enemies and slots for super weapon

---
+++ Assets/UserInterface/Scripts/SuperWeaponController.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SuperWeaponController : MonoBehaviour
+{
+    public Button[] slots;
+
+    public Button buyAirStrikeButton;
+
+    private int index = 0;
+
+    private void Start()
+    {
+        buyAirStrikeButton.onClick.AddListener(BuyAirStrike);
+    }
+
+    private void Update()
+    {
+        UseSuperWeapons();
+    }
+
+    private void BuyAirStrike()
+    {
+        if (index < slots.Length)
+        {
+            slots[index].transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "AS";
+            index += 1;
+        }
+        else
+        {
+        }
+    }
+
+    private void UseSuperWeapons()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            slots[0].transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "-";
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            slots[1].transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "-";
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            slots[2].transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "-";
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            slots[3].transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "-";
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            slots[4].transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "-";
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha6))
+        {
+            slots[5].transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "-";
+        }
+    }
+}
